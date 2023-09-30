f832d462d195bdc595d0a39f4131a63d57bffcc2
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-09-30 16:32:10 -0300

    Initial commit

---
+++ src/main/java/com/wandeilson/transitoapi/TransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.wandeilson.transitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitoApiApplication.class, args);
+	}
+
+}
